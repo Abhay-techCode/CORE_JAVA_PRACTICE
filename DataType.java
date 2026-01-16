9a284ab0df13e63e7954cb469e2ee708ae5e86a7
Abhay Patil <abhaycpatil2005@gmail.com>
2026-01-16 22:41:57 +0530

    Add files via upload
    Here the first thing that i do is i declare and initialize some of the variable then print then ....similarly i just declare a static variable and try to print do it prints a default value of each datatype ......static accessible directly non static req object .

---
+++ DataType.java
@@ -0,0 +1,26 @@
+public class DataType {
+    static byte b;
+    static short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+    static char c;
+    static boolean b1;
+    public static void main(String[] args) {
+     /*   byte b = 10;
+        short s = 1000;
+        int i = 1000;
+        long l = 100000000;
+        float f = 24.23F;
+        double d = 5689.00000;
+        char c = 'a';
+        boolean b1 = false;
+        System.out.println("The Different Datatypes represtation of Values");
+        System.out.println(b+"\n"+s+"\n"+i+"\n"+l+"\n"+f+"\n"+d+"\n"+c+"\n"+b1);   */
+        System.out.println("---------------------------------------");
+        System.out.println("Now Deals with Default values because in java there is no concept of Garbage Value");
+
+        System.out.println(b+"\n"+s+"\n"+i+"\n"+l+"\n"+f+"\n"+d+"\n"+c+"\n"+b1);
+    }
+}
