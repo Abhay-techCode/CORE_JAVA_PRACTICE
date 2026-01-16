3ce1bf73abc9531a83c304a3a4d986c1ac15fc2b
Abhay Patil <abhaycpatil2005@gmail.com>
2026-01-16 21:52:13 +0530

    Add files via upload
    Here what i do?
    i do multiple print statement use showcases likely number,variable,string etc and the combination of them

---
+++ MagicPrint.java
@@ -0,0 +1,15 @@
+public class MagicPrint {
+    public static void main(String args[]){
+
+        int x = 50;
+        System.out.println(x);
+        System.out.println(20+x);
+        System.out.println(20+20);
+        System.out.println("ABHAY"+20);
+        System.out.println(20+10+ "Abhay"+20+10);
+        System.out.println(20+10+ "Abhay"+(20+10));
+
+
+    }
+
+}
